d1fefe13cd1501b0d013628dd19b97e7e8099a5a
Emmanuel Atuahene <62813726+lilemman@users.noreply.github.com>
2025-01-03 14:03:33 -0500

    installed bootstrap and react router dom

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            EA <span className="text-sm text-slate-200">CODE</span>{" "}
+            <span className="text-3xl text-slate-700 ">X</span>
+          </Navbar.Brand>
+          <Nav className="mx-auto gap-7">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
